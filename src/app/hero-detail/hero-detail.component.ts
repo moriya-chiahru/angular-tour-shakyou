d6563ecab941cd8240bac817a3ffe1df3e42350c
守屋千春 <mry116k@hotmail.com>
2018-04-07 12:54:28 +0900

    herosコンポーネントの詳細表示をhero-detailコンポーネントに写す

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Hero } from '../hero';
+
 
 @Component({
   selector: 'app-hero-detail',
@@ -9,7 +12,10 @@ export class HeroDetailComponent implements OnInit {
 
   constructor() { }
 
+  @Input() hero: Hero; 
+
   ngOnInit() {
+
   }
 
 }
