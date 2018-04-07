4d521e5ce71eaeff61b58e66004a8c6064308622
守屋千春 <mry116k@hotmail.com>
2018-04-07 22:33:21 +0900

    新規追加を追加

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -52,6 +52,14 @@ export class HeroService {
     );
   }
 
+  /** POST: サーバーに新しいヒーローを登録する */
+  addHero (hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
   /**
    * 失敗したHttp操作を処理します。
    * アプリを持続させます。
