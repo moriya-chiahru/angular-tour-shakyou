094eda14a3bcd49687ceb56be5f78118faab52c2
守屋千春 <mry116k@hotmail.com>
2018-04-07 15:07:49 +0900

    app.moduleにHttpClientModuleを追記

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 
